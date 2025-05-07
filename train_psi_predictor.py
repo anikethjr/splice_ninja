b4eea0e07f9ed2047ee18fffcbde6892f8e3959f
Aniketh Janardhan Reddy <anikethjr@gmail.com>
2025-05-06 21:07:56 -0700

    Minor

--- train_psi_predictor.py
+++ train_psi_predictor.py
@@ -185,7 +185,7 @@ def main():
             # early_stopping_cb,
         ],
         precision="16-mixed",
-        strategy="ddp",
+        strategy="ddp" if n_gpus > 1 else "auto",
         reload_dataloaders_every_n_epochs=1,
     )
 
